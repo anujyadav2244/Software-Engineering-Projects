8d22b3cd2032e28e30b83c14e9504f8bbf5d1e74
anujyadav2244 <anujyadavjnp02@gmail.com>
2025-01-05 23:08:43 +0530

    first commit

---
+++ IPL/src/main/java/CRIC-TRACKER/IPL/data/JobCompletionNotificationListener.java
@@ -0,0 +1,41 @@
+package CRIC.TRACKER.IPL.DASHBOARD.data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener implements JobExecutionListener {
+
+  private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @SuppressWarnings("null")
+  @Override
+public void afterJob(JobExecution jobExecution) {
+    if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+        log.info("!!! JOB FINISHED! Time to verify the results");
+
+        try {
+            jdbcTemplate
+                .query("SELECT team1, team2, date FROM match", (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date " + rs.getString(3))
+                .forEach(str -> System.out.println(str));
+        } catch (DataAccessException e) {
+            log.error("Error querying the database: ", e);
+        }
+    } else {
+        log.error("!!! JOB FAILED! Batch job did not complete successfully.");
+    }
+}
+
+}
+  
