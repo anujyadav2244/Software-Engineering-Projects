6c8fef30d0857d86c563e0cf880dc52c34e3be69
anujyadav2244 <anujyadavjnp02@gmail.com>
2025-02-21 23:30:04 +0530

    Backend updated

--- IPL-Dashboard/src/main/java/crictracker/ipl_dashboard/controller/MatchController.java
+++ IPL-Dashboard/src/main/java/crictracker/ipl_dashboard/controller/MatchController.java
@@ -1,12 +1,17 @@
 package crictracker.ipl_dashboard.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
 
 import crictracker.ipl_dashboard.model.Match;
 import crictracker.ipl_dashboard.model.TeamMatchCount;
@@ -27,10 +32,20 @@ public class MatchController {
         this.customMatchRepository = customMatchRepository;
     }
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+
     @GetMapping("/team/{teamName}")
     public List<Match> getMatchesByTeam(@PathVariable String teamName) {
-        return matchRepository.findByTeam1OrTeam2(teamName, teamName);
+        List<Match> matches = matchRepository.findByTeam1OrTeam2(teamName, Pageable.unpaged()).getContent();
+
+        // Convert String to LocalDate for sorting
+        return matches.stream()
+                .sorted((m1, m2) -> LocalDate.parse(m2.getDate(), FORMATTER)
+                        .compareTo(LocalDate.parse(m1.getDate(), FORMATTER)))
+                .collect(Collectors.toList());
     }
+
 
     @GetMapping("/total-matches")
     public List<TeamMatchCount> getTotalMatchesByTeam() {
--- IPL-Dashboard/src/main/java/crictracker/ipl_dashboard/repository/MatchRepository.java
+++ IPL-Dashboard/src/main/java/crictracker/ipl_dashboard/repository/MatchRepository.java
@@ -2,14 +2,18 @@ package crictracker.ipl_dashboard.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import crictracker.ipl_dashboard.model.Match;
 
 
 
 public interface MatchRepository extends MongoRepository<Match, String> {
-    List<Match> findByTeam1OrTeam2(String team1, String team2);
+    @Query("{ '$or': [ { 'team1': ?0 }, { 'team2': ?0 } ] }")
+    Page<Match> findByTeam1OrTeam2(String teamName, Pageable pageable);
 
     long countByTeam1OrTeam2(String team1, String team2);
 
